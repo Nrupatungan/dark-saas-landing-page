76e810b654499c3f3898a45df4effc39f906b631
Dinesh Sake <dineshsake12345@gmail.com>
2025-04-06 12:43:42 +0530

    Added translate animation on LogoTicker

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
@@ -5,6 +7,7 @@ import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
 import Image from "next/image";
+import { motion } from 'motion/react'
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -22,11 +25,24 @@ export const LogoTicker = () => {
       <div className="container">
         <h2 className="text-xl text-white/70 text-center">Trusted by the world&apos;s most innovative teams</h2>
         <div className="mt-9 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-[64px]">
+          <motion.div className="flex flex-none gap-16 pr-16"
+          initial = {{
+            translateX: "0%"
+          }}
+          animate = {{
+            translateX: "-50%"
+          }}
+          transition={{
+            duration: 30,
+            repeat: Infinity,
+            ease: "linear",
+            repeatType: "loop"
+          }}
+          >
             {[...images,...images].map((img, idx) => (
               <Image key={idx} src={img.src} alt={img.alt} height={32} className="max-w-full" />
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
