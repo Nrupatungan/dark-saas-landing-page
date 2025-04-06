4b48d848935ab584647eec48a293287c03645107
Dinesh Sake <dineshsake12345@gmail.com>
2025-04-06 12:25:58 +0530

    Added parallax and draggable effects

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,13 +1,49 @@
+"use client"
+
 import emojiStar from '@/assets/images/emojistar.png'
 import helixImage from '@/assets/images/helix2.png'
+import { useScroll, useTransform } from 'motion/react'
 import Image from "next/image"
+import { useRef } from 'react'
+import { motion } from 'motion/react'
 
 export const CallToAction = () => {
+  const sectionRef = useRef(null);
+
+  const {scrollYProgress} = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start']
+  })
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [200, -200]);
+
   return (
-    <section className="py-[72px] md:py-24 bg-black text-white">
+    <section className="py-[72px] md:py-24 bg-black text-white overflow-x-clip" ref={sectionRef}>
       <div className="container max-w-xl md:px-0 relative">
-        <Image src={emojiStar} alt="Pie Chart Image" height={200} width={200} className="absolute -top-[120px] right-[calc(100%+24px)]"/>
-        <Image src={helixImage} alt="Cog Wheel Image" height={225} width={225} className="absolute top-6 left-[calc(100%+36px)]"/>
+        <motion.div className="absolute -top-[120px] right-[calc(100%+24px)]" style={{
+          translateY
+        }}
+        drag
+        dragSnapToOrigin
+        dragConstraints={sectionRef}
+        >
+          <Image src={emojiStar} alt="Pie Chart Image" height={200} width={200} className='max-w-none'
+          draggable="false"
+          />
+        </motion.div>
+
+        <motion.div className="absolute top-6 left-[calc(100%+36px)]" style={{
+          translateY
+        }}
+        drag
+        dragSnapToOrigin
+        dragConstraints={sectionRef}
+        >
+          <Image src={helixImage} alt="Cog Wheel Image" height={225} width={225} className='max-w-none'
+          draggable="false"
+          />
+        </motion.div>
+        
         <h2 className="text-5xl md:text-6xl font-bold text-center tracking-tighter">Get instant access</h2>
         <p className="text-xl text-white/70 text-center mt-5">Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
 
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,11 +1,24 @@
+"use client"
+
 import ArrowIcon from '@/assets/icons/arrow-w.svg'
 import cursorImage from '@/assets/images/cursor.png'
 import messageImage from '@/assets/images/message.png'
 import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'motion/react'
+import { useRef } from 'react';
 
 export const Hero = () => {
+  const sectionRef = useRef(null);
+
+  const {scrollYProgress} = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start']
+  })
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [200, -200]);
+
   return (
-    <section className="bg-black text-white py-[72px] sm:py-24 bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] relative overflow-clip">
+    <section className="bg-black text-white py-[72px] sm:py-24 bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] relative overflow-clip" ref={sectionRef}>
 
       {/*Gradient Arc*/}
       <div className='absolute left-1/2 -translate-x-1/2 top-[calc(100%-96px)] sm:top-[calc(100%-120px)] h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)]'></div>
@@ -25,8 +38,30 @@ export const Hero = () => {
        <div className='flex justify-center mt-8'>
          <div className='inline-flex relative'>
             <h1 className='text-7xl sm:text-9xl font-bold tracking-tighter text-center  inline-flex'>One Task<br />at a Time</h1>
-            <Image src={cursorImage} alt='Cursor Image' height={200} width={200} className='absolute top-[108px] left-[-156px] max-md:hidden' />
-            <Image src={messageImage} alt='Message Image' height={200} width={200} className='absolute top-[56px] left-[500px] max-md:hidden' />
+            <motion.div className='absolute top-[108px] left-[-156px] max-md:hidden'
+            style={{
+              translateY
+            }}
+            drag
+            dragSnapToOrigin
+            >
+              <Image src={cursorImage} alt='Cursor Image' height={200} width={200} className='max-w-none'
+              draggable="false"
+              />
+            </motion.div>
+            
+            <motion.div className='absolute top-[56px] left-[500px] max-md:hidden'
+            style={{
+              translateY
+            }}
+            drag
+            dragSnapToOrigin
+            >
+              <Image src={messageImage} alt='Message Image' height={200} width={200} className='max-w-none'
+              draggable="false"
+              />
+
+            </motion.div>
          </div>
        </div>
         
